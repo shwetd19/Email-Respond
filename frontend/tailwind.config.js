062acd55800575c258f92a5b77e8c8b3bb571880
shwetd19 <shwetas.dhake@mitaoe.ac.in>
2024-02-10 21:09:32 +0530

    final

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,26 +1,11 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#EE9789",
-      },
-    },
-    fontFamily: {
-      sans: ["Montserrat", "sans-serif"],
-    },
-  },
-  plugins: [],
-};
-
-// tailwind.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [react()],
-});
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+	  "./index.html",
+	  "./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+  }
