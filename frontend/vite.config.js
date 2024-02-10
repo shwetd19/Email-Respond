bfd6d2a5c7e93037886902074b7af79eeaed0010
shwetd19 <shwetas.dhake@mitaoe.ac.in>
2024-02-10 21:17:26 +0530

    vite fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,25 @@
+import { defineConfig } from "vite";
+import ReactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: '/src/main.jsx',
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:10000",
+        secure: false,
+      },
+    },
+  },
+  plugins: [ReactRefresh(), react()],
+});
+
+
 // // vite.config.js
 // import ReactRefresh from "@vitejs/plugin-react-refresh";
 
@@ -13,15 +35,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc"; 
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:10000",
-        secure: false,
-      },
-    },
-  },
-  plugins: [react()],
-});
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:10000",
+//         secure: false,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// });
