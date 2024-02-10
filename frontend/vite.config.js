f6f36a99aa89d7a63b201c26f81e9fbe557abbce
shwetd19 <shwetas.dhake@mitaoe.ac.in>
2024-02-10 21:00:30 +0530

    final commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,14 +11,13 @@
 // };
 
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc"; 
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:10000",
         secure: false,
       },
     },
