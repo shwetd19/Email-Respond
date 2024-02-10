9b2711b6257bdcd5e322078561b216f5dd754dde
shwetd19 <shwetas.dhake@mitaoe.ac.in>
2024-02-10 19:57:06 +0530

    vite  config fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,3 +12,12 @@ module.exports = {
   },
   plugins: [],
 }
+
+// tailwind.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+});
+
