714c6ef78e88d1766589dd424e12cf9242f49e5b
shwetd19 <shwetas.dhake@mitaoe.ac.in>
2024-02-10 20:55:05 +0530

    last commit

--- backend/credentials.js
+++ backend/credentials.js
@@ -2,6 +2,8 @@ module.exports = {
   EMAIL: "16alone.boy@gmail.com",
 
   PASSWORD: "phtqtpsszvfhwlzi",
+
+  PORT : "10000"
 };
 
 //   PASSWORD: "phtq tpss zvfh wlzi",
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,22 +2,25 @@ module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
-		colors: {
-			primary: '#EE9789'
-		}
-	},
-	fontFamily: {
-		sans: ['Montserrat', 'sans-serif'],
-	}
+      colors: {
+        primary: "#EE9789",
+      },
+    },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
 
 // tailwind.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [react()],
 });
-
